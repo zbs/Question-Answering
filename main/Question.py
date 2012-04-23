e24aee586d87d0eb84a03852a57db4bd6083b1ad
Areeb <areeb.malik91@gmail.com>
2012-04-23 00:28:53 -0400

    Baseline done!

---
+++ main/Question.py
@@ -0,0 +1,37 @@
+import gzip
+from nltk import word_tokenize
+
+class Question():
+    def __init__(self,number,desc,docs):
+        self.number = number
+        self.desc = desc
+        self.docs = gzip.open(docs)
+    
+    def run_baseline(self):
+        q_tokens = word_tokenize(self.desc)[:-1] #remove "?"
+        best_match = []
+        best_score = 0
+        tokens = []
+        count = 0
+        for line in self.docs:
+            count += 1
+            if count > 1000: #gets thru over 10 documents
+                break
+            tokens += word_tokenize(line)
+        while tokens:
+            if len(tokens) > 10:
+                input = tokens[:10]
+                tokens = tokens[10:]
+            else:
+                input = tokens
+                tokens = []
+            score = 0
+            for q_token in q_tokens:
+                if q_token in input:
+                    score += 1
+            if score > best_score:
+                best_score = score
+                best_match = input
+        return ' '.join(best_match)
+        #return "Aleksei A. Leonov was the first dude"
+            
---
+++ main/run.py
@@ -0,0 +1,62 @@
+from Question import Question
+
+questions_file = "../questions.txt"
+answers_file = "../answers.txt"
+DOCS = "../docs/"
+
+# extracts data from questions file...
+def extract_questions(questions_file):
+    # key: question number
+    # value: question text
+    questions = {}
+    isText = False
+    qNumber = 0
+    qText = ""
+    for line in open(questions_file):
+        if line.startswith("</top>"):
+            questions[qNumber] = qText
+            qNumber = 0
+            qText = ""
+        elif line.startswith("<num>"):
+            qNumber = int(line.split()[-1])
+        elif line.startswith("<desc>"):
+            isText = True
+        elif isText:
+            qText = line[:-1]
+            isText = False
+    return questions
+
+def extract_answers(answers_file):
+    # key: question number
+    # value: answer text
+    answers = {}
+    answer_in = -1
+    for line in open(answers_file):
+        answer_in -= 1
+        if line.startswith("Question"):
+            answer_in = 3
+            qNumber = int(line.split()[-1])
+        elif not answer_in:
+            answers[qNumber] = line[:-1]
+    return answers
+    
+def main():
+    questions = extract_questions(questions_file)
+    answers = extract_answers(answers_file)
+    score = 0.0
+    for qNumber in questions:
+        docs = DOCS + "top_docs." + str(qNumber) + ".gz"
+        question = Question(qNumber,questions[qNumber],docs)
+        guess = question.run_baseline()
+        if answers[qNumber].lower() in guess.lower():
+            score +=1
+            print "got one!"
+            print qNumber
+            print answers[qNumber].lower()
+            print guess.lower()
+    accuracy = score / len(questions)
+    print "Correct Guesses: " + str(score)
+    print "Accuracy: " + str(accuracy)
+
+if __name__ == '__main__':
+    main()
